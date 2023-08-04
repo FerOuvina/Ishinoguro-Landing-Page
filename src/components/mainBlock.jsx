f30cc8e1737fc1d4ffeab7030a26d6f10273d17c
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-04 20:06:49 -0300

    Adapting component to TailwindCSS

--- src/components/mainBlock.jsx
+++ src/components/mainBlock.jsx
@@ -6,20 +6,21 @@ import '../stylesheets/header.css';
 
 const MainBlock = block(function Main() {
   return (
-    <section className='headerContainer'>
-      <article className='headerContent'>
+    <section className='headerContainer w-screen h-screen flex'>
+      <article className='headerContent flex flex-col justify-center gap-1 container w-[320px] sm:w-[400px] md:w-[650px]'>
         <div className='headerContent_Container'>
-          <header className='headerContent_header'>
-            <div className='headerContent_header-title'>
-              <h1>Ishinoguro</h1>
-              <nav className='headerContent_aboutMeContainer-nav'>
-                <ul>
+          <header className='headerContent_header flex flex-col justify-center h-[100px]'>
+            <div className='flex justify-between'>
+              <h1 className='text-4xl px-3'>Ishinoguro</h1>
+              <nav className='flex items-center justify-between'>
+                <ul className='flex gap-2 mr-2'>
                   <li>
                     <a
                       href='https://twitter.com/ishinoguro'
                       target='_blank'
                       referrerPolicy='no-referrer'
                       rel='noreferrer'
+                      className='flex flex-col justify-center items-center w-9 sm:w-10 md:w-12'
                     >
                       <img src={twitterIcon} alt='' title='Twitter' />
                     </a>
@@ -30,6 +31,7 @@ const MainBlock = block(function Main() {
                       target='_blank'
                       referrerPolicy='no-referrer'
                       rel='noreferrer'
+                      className='flex flex-col justify-center items-center w-9 sm:w-10 md:w-12'
                     >
                       <img src={curiousCatIcon} alt='' title='CuriousCat' />
                     </a>
@@ -37,52 +39,50 @@ const MainBlock = block(function Main() {
                 </ul>
               </nav>
             </div>
-            <h2 className='headerContent_header-sub'>
+            <h2 className='pl-8 sm:pl-10 md:pl-11'>
               Digital Artist, Freelance Illustrator
             </h2>
           </header>
 
           <hr className='headerContent_hr hr1' />
 
-          <div className='headerContent_aboutMeContainer'>
-            <aside className='headerContent_aboutMeContainer-illustration'>
-              <img src={Ilustration4} alt='waos' />
-            </aside>
+          <div className='flex flex-col'>
+            <div className='aboutMe_Container'>
+              <aside className=' md:flex md:justify-center md:items-center'>
+                <img
+                  src={Ilustration4}
+                  alt='Ilustration of a women'
+                  className='ilustration4 w-32 max-w-[250px] bg-white'
+                />
+              </aside>
 
-            <div className='headerContent_aboutMeContainer-content'>
-              <div>
-                <h2>・┆✦ About Me ✦ ┆・</h2>
-                <h3>Ishi • she/her • 20y. • 🇦🇷</h3>
-                <h4>sleepiest soldier fighting the biggest naps 💤</h4>
+              <div className='headerContent_aboutMeContainer-content flex flex-col gap-3'>
+                <div>
+                  <h2>・┆✦ About Me ✦ ┆・</h2>
+                  <h3>Ishi • she/her • 20y. • 🇦🇷</h3>
+                  <h4>sleepiest soldier fighting the biggest naps 💤</h4>
+                </div>
               </div>
-
-              <hr className='headerContent_hr' />
-
-              <div>
-                <span>
-                  ⭐Commission status: <em>open!</em>
-                </span>{' '}
-                <br />
-                <span>🎨Clip studio paint 🖌️Wacom One</span> <br />
-                <span>✍🏻Big fanartist 🎥I animate sometimes</span> <br />
-                <span>🔞I may post suggestive content</span> <br />
-                <span></span>
-                <span>
-                  I&apos;m learning to draw the fundamentals in my second
-                  twitter account
-                </span>
-              </div>
+            </div>
+            <hr className='headerContent_hr' />
+            <div>
+              <span>
+                ⭐Commission status: <em>open!</em>
+              </span>{' '}
+              <br />
+              <span>🎨Clip studio paint 🖌️Wacom One</span> <br />
+              <span>✍🏻Big fanartist 🎥I animate sometimes</span> <br />
+              <span>🔞I may post suggestive content</span> <br />
+              <span></span>
+              <span>
+                I&apos;m learning to draw the fundamentals in my second twitter
+                account
+              </span>
             </div>
           </div>
 
           <nav className='headerContent_nav'>
-            <ul>
-              <li>
-                <a href='#'>Home</a>
-              </li>
-              <li>
-                <a href='#'>Portfolio</a>
-              </li>
+            <ul className='flex gap-3 mx-4 flex-col text-center justify-evenly flex-shrink flex-1 flex-wrap'>
               <li>
                 <a href='#'>Contact</a>
               </li>
