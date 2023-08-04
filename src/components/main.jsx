8eeca3503a51a89cadf1d2fb727a9ac64eae6008
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-04 18:04:45 -0300

    main component for rendering the rest of the components

---
+++ src/components/main.jsx
@@ -0,0 +1,12 @@
+import { block } from 'million/react';
+import HeaderBlock from './header';
+
+const MainBlock = block(function Main() {
+  return (
+    <>
+      <HeaderBlock />
+    </>
+  );
+});
+
+export default MainBlock;
