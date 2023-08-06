81cd39e682752ed84b2a313c7dfc1324b8b6ca27
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-06 17:25:07 -0300

    Setting up github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import million from 'million/compiler';
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
- 
+
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
+  base: 'Ishinoguro-Landing-Page',
 });
