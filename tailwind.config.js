5bc9f33d86e8101cd1f259b674070020fb7a707e
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-10 18:22:48 -0300

    Adding a custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        custom: '2fr 1fr',
+      },
+    },
   },
   plugins: [],
 };
