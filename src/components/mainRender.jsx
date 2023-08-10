e51d6afe6a8705e574c61bb665540263950f2827
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-10 18:25:07 -0300

    Adding new component to the render

--- src/components/mainRender.jsx
+++ src/components/mainRender.jsx
@@ -1,12 +1,14 @@
 import { block } from 'million/react';
 import MainBlock from './mainBlock';
 import GalleryBlock from './galleryBlock';
+import ContactBlock from './contactBlock';
 
 const MainRenderBlock = block(function RenderBlock() {
   return (
     <>
       <MainBlock />
       <GalleryBlock />
+      <ContactBlock />
     </>
   );
 });
