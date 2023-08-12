d86d037b4142d0e998ce37385962a8561b568d54
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-11 23:13:22 -0300

    Component to display each sectiong of comissions using the dropdown component

---
+++ src/components/artworkBlock.jsx
@@ -0,0 +1,247 @@
+import { block } from 'million/react';
+import DropdownBlock from './DropdownBlock';
+import waos from '../assets/images/gallery/illustration2.png';
+import waos1 from '../assets/images/gallery/illustration4.png';
+import waos2 from '../assets/images/gallery/illustration6.png';
+import dividerPink from '../assets/images/dividerPink.gif';
+import '../stylesheets/comissionBlock.css';
+
+const ArtworkBlock = block(function Artwork() {
+  const sections = [
+    {
+      title: 'Chibis',
+      content: [
+        <article key={'halfBody-container'}>
+          <table className='w-full text-left border-collapse'>
+            <tbody>
+              <tr>
+                <th>Add-on</th>
+                <th>Price</th>
+              </tr>
+              <tr>
+                <td>Small Prop</td>
+                <td>$8</td>
+              </tr>
+              <tr>
+                <td>Big Prop/Furniture</td>
+                <td>$15</td>
+              </tr>
+              <tr>
+                <td>Extra Character</td>
+                <td>70%</td>
+              </tr>
+            </tbody>
+          </table>
+          <span>
+            Background it can be a plain color, a simple pattern or transparent
+          </span>
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Sketch</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Base Color</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos1} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Full Render</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos2} alt='' />
+        </article>,
+      ],
+    },
+    {
+      title: 'Icon/Bust',
+      content: [
+        <article key={'halfBody-container'}>
+          <div>
+            <table className='w-full text-left border-collapse'>
+              <tbody>
+                <tr>
+                  <th>Add-on</th>
+                  <th>Price</th>
+                </tr>
+                <tr>
+                  <td>Small Prop</td>
+                  <td>$8</td>
+                </tr>
+                <tr>
+                  <td>Big Prop/Furniture</td>
+                  <td>$15</td>
+                </tr>
+                <tr>
+                  <td>Extra Character</td>
+                  <td>70%</td>
+                </tr>
+              </tbody>
+            </table>
+            <span>
+              Background it can be a plain color, a simple pattern or
+              transparent
+            </span>
+          </div>
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Sketch</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Base Color</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos1} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Full Render</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos2} alt='' />
+        </article>,
+      ],
+    },
+    {
+      title: 'Half Body',
+      content: [
+        <article key={'halfBody-container'}>
+          <table className='w-full text-left border-collapse'>
+            <tbody>
+              <tr>
+                <th>Add-on</th>
+                <th>Price</th>
+              </tr>
+              <tr>
+                <td>Small Prop</td>
+                <td>$8</td>
+              </tr>
+              <tr>
+                <td>Big Prop/Furniture</td>
+                <td>$15</td>
+              </tr>
+              <tr>
+                <td>Extra Character</td>
+                <td>70%</td>
+              </tr>
+              <tr>
+                <td>Animals/Pets</td>
+                <td>75%</td>
+              </tr>
+              <tr>
+                <td>Background</td>
+                <td>90%</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Sketch</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Base Color</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos1} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Full Render</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos2} alt='' />
+        </article>,
+      ],
+    },
+    {
+      title: 'Full Body',
+      content: [
+        <article key={'halfBody-container'}>
+          <table className='w-full text-left border-collapse'>
+            <tbody>
+              <tr>
+                <th>Add-on</th>
+                <th>Price</th>
+              </tr>
+              <tr>
+                <td>Small Prop</td>
+                <td>$8</td>
+              </tr>
+              <tr>
+                <td>Big Prop/Furniture</td>
+                <td>$15</td>
+              </tr>
+              <tr>
+                <td>Extra Character</td>
+                <td>70%</td>
+              </tr>
+              <tr>
+                <td>Animals/Pets</td>
+                <td>75%</td>
+              </tr>
+              <tr>
+                <td>Background</td>
+                <td>90%</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Sketch</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Base Color</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos1} alt='' />
+
+          <div className='flex justify-center w-full'>
+            <img src={dividerPink} alt='Divider' className='mb-2 w-full' />
+          </div>
+          <h1 className='artwork-title'>Full Render</h1>
+          <h2 className='artwork-subtitle'>Starts from $10</h2>
+          <img src={waos2} alt='' />
+        </article>,
+      ],
+    },
+  ];
+  return (
+    <>
+      <h1 className='w-full text-2xl min-[480px]:text-3xl md:text-5xl text-[#524f50] bg-white py-4 text-center'>
+        <strong>━ ˖°˖ ☾</strong> O p t i o n s <strong> ˖°˖ ━</strong>
+      </h1>
+      <div className='px-4 mt-4 min-[480px]:inline-block min-[480px]:columns-2 w-full artwork-container lg:px-16 lg:gap-10'>
+        {sections.map((section, index) => (
+          <DropdownBlock
+            key={index}
+            title={section.title}
+            content={section.content}
+          />
+        ))}
+      </div>
+    </>
+  );
+});
+
+export default ArtworkBlock;
