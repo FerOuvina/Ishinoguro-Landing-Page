6cd4bf76480f25f6d909aeb3202fd7fa56fd3732
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-15 20:25:01 -0300

    Adding the component to the render

--- src/components/comissionBlock.jsx
+++ src/components/comissionBlock.jsx
@@ -1,6 +1,7 @@
 import { block } from 'million/react';
 import ArtworkBlock from './artworkBlock';
 import TOSBlock from './TOSBlock';
+import CommissionFormBlock from './comissionFormBlock';
 import '../stylesheets/comissionBlock.css'
 
 const ComissionBlock = block(function Comission() {
@@ -8,6 +9,7 @@ const ComissionBlock = block(function Comission() {
     <div className='comissionContainer'>
       <TOSBlock />
       <ArtworkBlock />
+      <CommissionFormBlock />
     </div>
   );
 });
