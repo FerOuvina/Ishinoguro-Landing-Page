51dfce689ddc4a254e23070339816e5ac937d8a9
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-18 18:12:30 -0300

    Fixing navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Route } from 'wouter';
 import MainRenderBlock from './components/mainRender';
-import TOSBlock from './components/tosBlock';
 import ComissionBlock from './components/comissionBlock';
 import './App.css';
 
@@ -8,8 +7,7 @@ function App() {
   return (
     <>
       <Route path='/' component={MainRenderBlock} />
-      <Route path='/TOS' component={TOSBlock} />
-      <Route path='/Comissions' component={ComissionBlock} />
+      <Route path='/commissions' component={ComissionBlock} />
     </>
   );
 }
