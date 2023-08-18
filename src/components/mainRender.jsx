ccc58f906d2c641fb0c6f6b60e63ef0ae2eb3cfb
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-18 18:11:47 -0300

    Reverting component

--- src/components/mainRender.jsx
+++ src/components/mainRender.jsx
@@ -1,16 +1,15 @@
 import { block } from 'million/react';
-import { Route } from 'wouter';
 import MainBlock from './mainBlock';
 import GalleryBlock from './galleryBlock';
 import ContactBlock from './contactBlock';
 
 const MainRenderBlock = block(function RenderBlock() {
   return (
-    <Route path='/'>
+    <>
       <MainBlock />
       <GalleryBlock />
       <ContactBlock />
-    </Route>
+    </>
   );
 });
 
