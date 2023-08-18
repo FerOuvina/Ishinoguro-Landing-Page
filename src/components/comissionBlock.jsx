cece6e980a4bfb5bf475a789eb6bfaa888fc1da3
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-18 18:12:14 -0300

    Removing unnecesary use of wouter on component

--- src/components/comissionBlock.jsx
+++ src/components/comissionBlock.jsx
@@ -1,5 +1,4 @@
 import { block } from 'million/react';
-import { Route } from 'wouter';
 import ArtworkBlock from './artworkBlock';
 import TOSBlock from './TOSBlock';
 import CommissionFormBlock from './comissionFormBlock';
@@ -7,13 +6,11 @@ import '../stylesheets/comissionBlock.css';
 
 const ComissionBlock = block(function Comission() {
   return (
-    <Route path='/comissions'>
-      <div className='comissionContainer'>
-        <TOSBlock />
-        <ArtworkBlock />
-        <CommissionFormBlock />
-      </div>
-    </Route>
+    <div className='comissionContainer'>
+      <TOSBlock />
+      <ArtworkBlock />
+      <CommissionFormBlock />
+    </div>
   );
 });
 
