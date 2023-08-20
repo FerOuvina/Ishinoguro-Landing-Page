1a1383e862a96a4cbfc2678c82914fbf76c9e196
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-08-20 20:45:54 -0300

    Adding the new location for the thankyou component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route } from 'wouter';
 import MainRenderBlock from './components/mainRender';
 import ComissionBlock from './components/comissionBlock';
+import ThankyouBlock from './components/thankyou';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Route path='/' component={MainRenderBlock} />
       <Route path='/commissions' component={ComissionBlock} />
+      <Route path='/thankyou' component={ThankyouBlock} />
     </>
   );
 }
